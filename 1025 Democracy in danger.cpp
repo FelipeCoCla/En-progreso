0d22eca9a95e3820b4b517ce896b7df0217beccc
FelipeCoCla <42448123+FelipeCoCla@users.noreply.github.com>
2018-09-13 13:52:17 -0300

    Update 1025 Democracy in danger.cpp

--- 1025 Democracy in danger.cpp
+++ 1025 Democracy in danger.cpp
@@ -7,17 +7,18 @@ int main()
 {
     int c, menorpos, minimo;
     int suma = 0;
-    vector<int> vec;
+    vector<int> v;
     cin >> c;
     for (int i=0; i<=(c-1); i++) {
 
-        cin >> vec[i];
+        cin >> v[i];
     }
     c = (c+1)/2;
     for (int j=0; j<=(c-1),j++){
 
-        int menorpos = min_element(vec.begin(), vec.end()) - vec.begin();
-        minimo = min_element(vec);
+        int menorpos = std::min_element(v.begin(),v.end()) - v.begin();
+        int minimo = *std::min_element(v.begin(), v.end());
+
         suma += (minimo+1)/2;
         v[menorpos] = 10000;
 
